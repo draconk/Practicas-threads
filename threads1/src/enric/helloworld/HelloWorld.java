ee822373f8c16bc46be50a6c14f8c27184dc6d4d
draconk <draconk@gmail.com>
2014-12-05 11:41:07 +0100

    05/12/2014

--- threads1/src/enric/helloworld/HelloWorld.java
+++ threads1/src/enric/helloworld/HelloWorld.java
@@ -1,3 +1,5 @@
+package enric.helloworld;
+
 /**
  * Created by Enric on 04/12/2014.
  */
@@ -6,6 +8,6 @@ public class HelloWorld extends Thread{
     @Override
     public void run() {
         System.out.println("Hello world " + Thread.currentThread());
-        
+
     }
 }
---
+++ threads1/src/enric/wa_tor/Tiburon.java
@@ -0,0 +1,14 @@
+package enric.wa_tor;
+
+/**
+ * Created by Enric on 04/12/2014.
+ */
+public class Tiburon implements Runnable {
+
+    private int sex;
+
+
+    public void run() {
+
+    }
+}
