a21636f2519ae30dce524289624e279c3faaf67a
draconk <draconk@gmail.com>
2014-12-10 11:42:44 +0100

    Update del 09/12/2014

--- threads1/src/enric/wa_tor/Hilos.java
+++ threads1/src/enric/wa_tor/Hilos.java
@@ -13,7 +13,72 @@ public class Hilos extends Thread{
         this.esp = s;
         this.x = x;
         this.y = y;
+
+        Mundo.warudo[x][y] = this;
     }
 
+    public void movimiento(){
+        int mov = (int) (Math.random() * (4 - 1 + 1) + 1);
+
+        // 1 es X positivo
+        // 2 es X negativo
+        // 3 es Y positivo
+        // 4 es Y negativoç
+
+        switch (mov){
+            case 1 :
+                if (x == Mundo.mundo_X - 1){
+                    x = 0;
+                }else{
+                    x++;
+                }
+                break;
+            case 2 :
+                if (x == 0){
+                    x = Mundo.mundo_X - 1;
+                } else {
+                    x--;
+                }
+                break;
+            case 3 :
+                if (y == Mundo.mundo_Y - 1){
+                    y = 0;
+                }else{
+                    y++;
+                }
+                break;
+            case 4 :
+                if (y == 0){
+                    y = Mundo.mundo_Y - 1;
+                } else {
+                    y--;
+                }
+                break;
+
+        }
+    }
+
+    public final void run(){
+        while (true){
+
+            try {
+                Thread.sleep(200);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            movimiento();
+
+            Hilos aux = Mundo.warudo[x][y];
+
+            if (aux != null){
+                if (aux.esp.nombre.equals(esp.nombre) && aux.esp.sexo.equals(esp.sexo)){
+                    System.out.println("Ha morido un " + esp.nombre + " de sexo " + esp.sexo);
+                    //voy por aqui
+                }
+            }
+
+        }
+    }
 
 }
--- threads1/src/enric/wa_tor/Mundo.java
+++ threads1/src/enric/wa_tor/Mundo.java
@@ -5,12 +5,14 @@ package enric.wa_tor;
  */
 public class Mundo {
 
-    int mundo_X = 10;
-    int mundo_Y = 10;
-    int atunF_num = 10;
-    int atunM_num = 10;
-    int tiburF_num = 2;
-    int tiburM_num = 2;
+    static int mundo_X = 10;
+    static int mundo_Y = 10;
+    static int atunF_num = 10;
+    static int atunM_num = 10;
+    static int tiburF_num = 2;
+    static int tiburM_num = 2;
+
+    static Hilos[][] warudo = new Hilos[mundo_X][mundo_Y]; //el nombre de la variable es una referencia a Jo Jo's Bizzarre Adventure no es una typo
 
 
 }
