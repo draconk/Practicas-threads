7f2857dc2fc901e1a43027da71b0f82d9c47cb35
draconk <draconk@gmail.com>
2014-12-22 11:33:44 +0100

    Create Contador.java

---
+++ threads1/src/AMS2_04_14/sumador/Contador.java
@@ -0,0 +1,18 @@
+package AMS2_04_14.sumador;
+
+/**
+ * Created by Enric on 22/12/2014.
+ */
+public class Contador extends Thread{
+
+    public Contador(){}
+
+    public void run(){
+        for (int n = 0; n < 5000; n++){
+            Sumador_main.cont++;
+
+        }
+
+    }
+
+}
